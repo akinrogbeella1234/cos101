3a1ce59ebd9ac1dfe6c82f41ceafda8cc60abdab
akinrogbeella1234 <akinrogbekolade@gmail.com>
2024-11-18 16:56:46 +0100

    Add files via upload

---
+++ ella.py
@@ -0,0 +1,29 @@
+S
+def ella(length,breath):
+    area =length*breath
+    print(f"the area is{area}cm^2")
+ella(30,40)
+
+def a(length,breath,height):
+    volume =length*breath*height
+    print(f"the volume={volume}cm^3")
+a(30,40,50)
+def b(speed,time):
+    distance =speed/time
+    print(f"distance={distance}m")
+b(40,2)
+def c(power,time):
+    energy =power*time
+    print(f"energy={energy}js")
+c(29,33)
+
+def d(density,acceleration,height):
+    pressure =density*acceleration*height
+    print(f"pressure={pressure}Nm^2")
+d(9,10,15)
+def e(mass,volume):
+    mole =mass/volume
+    print(f"mole={mole}kgm^3")
+e(40,5)
+
+
